fefe3d77c59230ef962eac8ff8c3cd89863bcb6f
Joshua Suresh <jsuresh@cfa.harvard.edu>
2014-08-19 11:47:18 -0400

    Continuing work on utilities.

--- fan.py
+++ fan.py
@@ -171,9 +171,14 @@ class full_analysis:
     # 2D data analysis functions #
     ##############################
 
-    ####################
-    # Data Preparation #
-    ####################
+    #############
+    # Utilities #
+    #############
+    # self.find_desired_grpids
+    # self.load_grid_data
+    # self._grid_to_kpc
+    # self.get_gal_props
+
     def dat_prep(self,redshifts,c0_128=0,c0_256=0,c0_512=0,c0_fw_256=0,c0_sw_256=0,c2_256=0,c3_512=0,c4_512=0,c4_check=0,g0_BH=0,g10_BH=0,g20_BH=0,g30_BH=0,g40_BH=0,g50_BH=0,g25_BH=0,g75_BH=0,g95_BH=0,g0_noBH=0,g10_noBH=0,g20_noBH=0,g30_noBH=0,g40_noBH=0,g50_noBH=0,g25_noBH=0,g75_noBH=0,g95_noBH=0,g10_nothermal=0,g20_nothermal=0,g30_nothermal=0,g40_nothermal=0,g50_nothermal=0,g25_fixv=0,g50_fixv=0,g25_fixv_fixeta=0,g50_fixv_fixeta=0,g25_fixv_nothermal=0,g50_fixv_nothermal=0,c0_nometalwinds=0,c0_fullmetalwinds=0):
         # self.run_list = []
         # self.color_list = [] 
@@ -182,17 +187,101 @@ class full_analysis:
         # self.snapnum_list = [] 
         # self.snapdir_list = []
 
-        if c0_128 == 1:
+        self.mv_base = "/n/hernquistfs1/mvogelsberger/projects/GFM/Production/Cosmo/"
+        self.sb_base = "/n/hernquistfs1/spb/Cosmo/"
+        self.js_base = "/n/hernquistfs1/jsuresh/Runs/"
+
+        if c0_256 == 1:
             for redshift in redshifts:
-                self.run_list.append('c0_128')
+                self.run_list.append('c0_256')
                 self.color_list.append('blue')
-                self.label_list.append('Cosmo0 (128)')
+                self.label_list.append('Fiducial')
                 self.linestyle_list.append('solid')
-                if 
-                self.snapnum_list
+                self.snapdir_list.append(self.mv_base+"Cosmo0_V6/L25_n256")
+                if redshift == '4': self.snapnum_list.append(54)
+                if redshift == '3': self.snapnum_list.append(60)
+                if redshift == '2': self.snapnum_list.append(68)
+        if c0_fw_256 == 1:
+            for redshift in redshifts:
+                self.run_list.append('c0_fw_256')
+                self.color_list.append('blue')
+                self.label_list.append('Fast Winds')
+                self.linestyle_list.append('solid')
+                self.snapdir_list.append(self.mv_base+"Cosmo0_V6/L25_n256")
+                if redshift == '4': self.snapnum_list.append(54)
+                if redshift == '3': self.snapnum_list.append(60)
+                if redshift == '2': self.snapnum_list.append(68)
+        if c0_sw_256 == 1:
+            for redshift in redshifts:
+                self.run_list.append('c0_sw_256')
+                self.color_list.append('blue')
+                self.label_list.append('Strong Winds')
+                self.linestyle_list.append('solid')
+                self.snapdir_list.append(self.mv_base+"Cosmo0_V6/L25_n256")
+                if redshift == '4': self.snapnum_list.append(54)
+                if redshift == '3': self.snapnum_list.append(60)
+                if redshift == '2': self.snapnum_list.append(68)
+        if c0_fw_256 == 1:
+            for redshift in redshifts:
+                self.run_list.append('c0_fw_256')
+                self.color_list.append('blue')
+                self.label_list.append('Fast Winds')
+                self.linestyle_list.append('solid')
+                self.snapdir_list.append(self.mv_base+"Cosmo0_V6/L25_n256")
+                if redshift == '4': self.snapnum_list.append(54)
+                if redshift == '3': self.snapnum_list.append(60)
+                if redshift == '2': self.snapnum_list.append(68)
+        if c2_256 == 1:
+            for redshift in redshifts:
+                self.run_list.append('c2_256')
+                self.color_list.append('blue')
+                self.label_list.append('No AGN')
+                self.linestyle_list.append('solid')
+                self.snapdir_list.append(self.mv_base+"Cosmo2_V6/L25_n256")
+                if redshift == '4': self.snapnum_list.append(54)
+                if redshift == '3': self.snapnum_list.append(60)
+                if redshift == '2': self.snapnum_list.append(68)
+        if c2_256 == 1:
+            for redshift in redshifts:
+                self.run_list.append('c2_256')
+                self.color_list.append('blue')
+                self.label_list.append('No AGN')
+                self.linestyle_list.append('solid')
+                self.snapdir_list.append(self.mv_base+"Cosmo2_V6/L25_n256")
+                if redshift == '4': self.snapnum_list.append(54)
+                if redshift == '3': self.snapnum_list.append(60)
+                if redshift == '2': self.snapnum_list.append(68)
 
+        if g0_BH == 1:
+            for redshift in redshifts:
+                self.run_list.append('c0_256')
+                self.color_list.append('blue')
+                self.label_list.append('Fiducial')
+                self.linestyle_list.append('solid')
+                self.snapdir_list.append(self.mv_base+"Cosmo0_V6/L25_n256")
+                if redshift == '4': self.snapnum_list.append(54)
+                if redshift == '3': self.snapnum_list.append(60)
+                if redshift == '2': self.snapnum_list.append(68)
 
+        if g50_fixv_nothermal == 1:
+            for redshift in redshifts:
+                self.run_list.append('g50_fixv_nothermal')
+                self.color_list.append('blue')
+                self.label_list.append('Fiducial')
+                self.linestyle_list.append('solid')
+                self.snapdir_list.append(self.mv_base+"Cosmo0_V6/L25_n256")
+                if redshift == '4': self.snapnum_list.append(54)
+                if redshift == '3': self.snapnum_list.append(60)
+                if redshift == '2': self.snapnum_list.append(68)
 
+        # self.dat_prep(redshifts,g0_BH=1)
+        # self.dat_prep(redshifts,c2_256=1)
+        # self.dat_prep(redshifts,c0_sw_256=1)
+        # self.dat_prep(redshifts,g50_fixv_nothermal=1)
+        # self.dat_prep(redshifts,g50_BH=1)
+        # self.dat_prep(redshifts,g50_fixv=1)
+        # self.dat_prep(redshifts,c0_nometalwinds=1)
+        # self.dat_prep(redshifts,c0_fullmetalwinds=1)
 
 
 
